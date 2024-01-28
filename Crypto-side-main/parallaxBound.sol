d2455ee8999accf612519c6ffc38669e66801984
Bound Finance <140350394+BoundFinance@users.noreply.github.com>
2024-01-28 14:07:42 +0000

    Update parallaxBound.sol

--- Crypto-side-main/parallaxBound.sol
+++ Crypto-side-main/parallaxBound.sol
@@ -14,6 +14,7 @@ contract ParallaxBound is DSAuth, DSNote {
 
     IERC20 public stableToken;
     IERC20 public WETH;
+    IWETH public weth;
     ICALMRouter calmRouter;
     IUniswapV3SwapRouter public uniswapRouter;
     IUniswapV3Pool public uniswapPool;
@@ -22,14 +23,20 @@ contract ParallaxBound is DSAuth, DSNote {
     uint public totalDepositedAsset;
     uint public reserveamountpercentage;
     uint public protocolshareofexcess;
+    uint public ILcover;
+    uint public RescueAmount;
+    uint public Cashbackshareofexcess;
     uint public RewardLedger;
     uint public cashbackappshare;
     uint public totalDepositedAssetUSD;
     uint256 public totalExcessReserve;
     uint256 constant internal magnitude = 2**128;
     uint256 public magnifiedExcessPerShare;
+    bool public _paused = false;
+    bool public _rescue = false;
     mapping(address => int256) public magnifiedExcessCorrections;
     mapping(address => uint256) public shares;
+    mapping(address => uint256) public sharesReal;
     mapping(address => uint256) public withdrawnExcess;
     mapping(address => uint256) public sharesUSD;
     mapping(address => uint256) public BackUpbalanceCount;
@@ -51,10 +58,21 @@ contract ParallaxBound is DSAuth, DSNote {
     constructor(address _erc20Address, address _cashback, address _protocol, address _WETH, address _router, address _poolAddress) {
         stableToken = IERC20(_erc20Address);
         WETH = IERC20(_WETH);
+        weth = IWETH(_WETH);
         cashbackapp = _cashback;
         protocol = _protocol;
         calmRouter = ICALMRouter(_router);
         uniswapPool = IUniswapV3Pool(_poolAddress);
+    }
+
+    modifier whenNotPaused() {
+        require(!_paused, "Contract is paused");
+        _;
+    }
+
+    modifier whenPaused() {
+        require(_paused, "Contract is not paused");
+        _;
     }
 
     function setemission(address _bckgovemissions) external auth {
@@ -77,10 +95,25 @@ contract ParallaxBound is DSAuth, DSNote {
         return totalDepositedAsset;
     }
 
-    function setPercentages(uint _percentage, uint _protocolSharePercent) public auth {
+        
+    function pause() public auth whenNotPaused {
+        _paused = true;
+        
+    }
+
+    
+    function unpause() public auth whenPaused {
+        _paused = false;
+        
+    }
+
+    //60% _percentage, 25% protocolshareofExcess, 25% _cashbackshare, ILcover 50%
+
+    function setPercentages(uint _percentage, uint _protocolSharePercent, uint _ILcover, uint _cashbackshare) public auth {
         require( _percentage < 100 && _protocolSharePercent < 100);
         reserveamountpercentage = _percentage;
         protocolshareofexcess = _protocolSharePercent;
+        Cashbackshareofexcess = _cashbackshare;
 
     }
     /* The deposit functions, first transfers the WETH to the contract, the contract then swaps WETH
@@ -89,14 +122,20 @@ contract ParallaxBound is DSAuth, DSNote {
        address in orcale.
      */
 
-    function depositsWETH(uint256 amount) public {
+
+    function depositsWETH(uint256 amount) public whenNotPaused {
         convertExcess();
+        (bool isPaused) = calmRouter.paused();
+        require(isPaused == false, "TVL reached its Max");
         require(amount > 0, "Amount must be greater than 0");
         require(WETH.balanceOf(msg.sender) >= amount, "Insufficient WETH balance");
         address user = msg.sender;
 
         WETH.transferFrom(msg.sender, address(this), amount);
-        WETH.approve(address(calmRouter), amount);
+        uint AmountAdjusted = (amount * 99/100);
+        uint AmounttoAdd = amount - AmountAdjusted;
+        ILcover += AmounttoAdd;
+        WETH.approve(address(calmRouter), AmountAdjusted);
         
         ICALMRouter.AssetConfig memory config = ICALMRouter.AssetConfig({
             pair: ICALMCore.Pair({
@@ -108,7 +147,7 @@ contract ParallaxBound is DSAuth, DSNote {
 
         ICALMCore.Amounts memory depositedAmounts = calmRouter.depositERC20Token(
             config, 
-            amount, 
+            AmountAdjusted, 
             [uint256(0), 0, 0, 0],
             0
         );
@@ -126,10 +165,11 @@ contract ParallaxBound is DSAuth, DSNote {
     [uint256(0), uint256(0), uint256(0), uint256(0)] // Corrected array with uint256 types
     );
 
-        uint amountUSD = ethToUsdc(amount);
-        totalDepositedAsset += amount;
-        shares[msg.sender] += amount;
-        BackUpbalanceCount[msg.sender] += amount;
+        uint amountUSD = ethToUsdc(AmountAdjusted);
+        totalDepositedAsset += AmountAdjusted;
+        shares[msg.sender] += AmountAdjusted;
+        sharesReal[msg.sender] += amount;
+        BackUpbalanceCount[msg.sender] += AmountAdjusted;
         sharesUSD[msg.sender] += amountUSD;
         totalDepositedAssetUSD += amountUSD;
 
@@ -137,17 +177,17 @@ contract ParallaxBound is DSAuth, DSNote {
         globals.globaltotaldepositadd(amountUSD);
 
         magnifiedExcessCorrections[msg.sender] = magnifiedExcessCorrections[msg.sender]
-            .sub((magnifiedExcessPerShare.mul(amount)).toInt256Safe());
+            .sub((magnifiedExcessPerShare.mul(AmountAdjusted)).toInt256Safe());
 
         emmission.updateInterestCorrectionssub(user, amountUSD);
 
         if (!hasDeposited[msg.sender]) {
         hasDeposited[msg.sender] = true;
-        emit NewDepositor(msg.sender, amount);
+        emit NewDepositor(msg.sender, AmountAdjusted);
         }
 
 
-        emit Deposited(msg.sender, amount);
+        emit Deposited(msg.sender, AmountAdjusted);
     }
 
 
@@ -155,7 +195,7 @@ contract ParallaxBound is DSAuth, DSNote {
     to user. 
      */
 
-    function exitETH() public {
+    function exitETH() public whenNotPaused {
         convertExcess();
         OrcaleuserShares(msg.sender);
 
@@ -180,9 +220,20 @@ contract ParallaxBound is DSAuth, DSNote {
 
         uint256 additionalWETH = swap(amountsWithdrawn.amount1, 0, token1, token0);
         uint256 totalWETH = amountsWithdrawn.amount0 + additionalWETH;
-        require(WETH.transferFrom(address(this), msg.sender, totalWETH), "WETH transfer failed");
+       
+        uint AdjustedDepositAmount = shares[msg.sender];
 
+        if(totalWETH < AdjustedDepositAmount) {
+        uint ILcoverAmount = AdjustedDepositAmount - totalWETH;
+        uint amountFromILCover = (ILcoverAmount > ILcover) ? ILcover : ILcoverAmount;
+        uint SendtoUser = totalWETH + amountFromILCover;
     
+        ILcover -= amountFromILCover;  // Deduct the used IL cover from the reserve
+        require(WETH.transferFrom(address(this), msg.sender, SendtoUser), "WETH transfer failed");
+        } else {
+        require(WETH.transferFrom(address(this), msg.sender, totalWETH), "WETH transfer failed");
+         }
+
         totalDepositedAsset -= userShares;
         shares[msg.sender] -= userShares;
 
@@ -197,6 +248,56 @@ contract ParallaxBound is DSAuth, DSNote {
         emmission.updateInterestCorrectionsadd(msg.sender, amountUSD);
 
         emit Exited(msg.sender, totalWETH);
+    }
+
+// make sure that 
+
+    function Rescue() public auth {
+       address token0 = address(WETH);
+       address token1 = address(stableToken);
+        (uint256 totalContractShares, , , , ) = calmRouter.userInfo(address(this));
+
+        ICALMCore.Pair memory pair = ICALMCore.Pair({token0: token0, token1: token1});
+
+        ICALMCore.Amounts memory amountsWithdrawn = calmRouter.withdrawTokens(
+            pair,
+            uint128(totalContractShares),
+            [uint256(0), uint256(0), uint256(0), uint256(0)], // uint256[3] array initialized with zeros
+            [uint256(0), uint256(0), uint256(0)] // amountOutMinOuter as a single uint256 value
+        );
+
+        uint256 additionalWETH = swap(amountsWithdrawn.amount1, 0, token1, token0);
+        uint256 totalWETH = amountsWithdrawn.amount0 + additionalWETH;
+        RescueAmount += totalWETH;
+        _rescue = true;
+    }
+
+    function retrieveDeposit() public {
+        require(_rescue == true, "Rescue has not been activated");
+        convertExcess();
+        OrcaleuserShares(msg.sender);
+
+        uint256 userShares = shares[msg.sender];
+        uint256 amountUSD = sharesUSD[msg.sender];
+        require(userShares > 0, "You don't have enough shares to withdraw");
+        withdrawExcess();
+
+        uint256 userShareOfPool = (userShares * RescueAmount) / totalDepositedAsset;
+
+        WETH.transferFrom(address(this), msg.sender, userShareOfPool);
+
+        totalDepositedAsset -= userShares;
+        shares[msg.sender] -= userShares;
+        RescueAmount -= userShareOfPool;
+        globals.globalsharessub(msg.sender, amountUSD); 
+        globals.globaltotaldepositsub(amountUSD);
+
+        totalDepositedAssetUSD -= amountUSD;
+        sharesUSD[msg.sender] -= amountUSD;
+
+        magnifiedExcessCorrections[msg.sender] = magnifiedExcessCorrections[msg.sender]
+            .add((magnifiedExcessPerShare.mul(userShares)).toInt256Safe());
+        emmission.updateInterestCorrectionsadd(msg.sender, amountUSD);
     }
 
     function calculateWithdrawalAmount(uint256 userShares) public view returns (uint256) {
@@ -237,6 +338,9 @@ contract ParallaxBound is DSAuth, DSNote {
         require(excessAmount >= 0, "excess must be greater than 0");
         uint256 reserveForUsers = (excessAmount * reserveamountpercentage) / 100;
 
+        address token0 = address(WETH);
+        address token1 = address(stableToken);
+
         totalExcessReserve += reserveForUsers;
         emit DistributedToReserve(block.timestamp, reserveForUsers);
 
@@ -247,12 +351,15 @@ contract ParallaxBound is DSAuth, DSNote {
         }
         uint amountForConversion1 = excessAmount - reserveForUsers;
 
-        uint cashbackappreward = amountForConversion1 * ((100 - protocolshareofexcess) / 100);
+        uint protocolshare = amountForConversion1 * ((protocolshareofexcess) / 100);
+        uint cashbackappreward = amountForConversion1 * ((Cashbackshareofexcess) / 100); // 25% for protocol 25% for cashback 50% for IL.
         cashbackappshare += cashbackappreward;
-        uint protocolreward = amountForConversion1 - cashbackappreward;
+        uint ILcoverAmount = amountForConversion1 - (cashbackappreward + protocolshare);
+        stableToken.approve(address(cash), cashbackappreward);
         cash.depositUSDC(cashbackappreward);
-
-        stableToken.transferFrom(address(this), address(protocol), protocolreward);
+        stableToken.transferFrom(address(this), address(protocol), protocolshare);
+        uint256 usdctoWETHConversion = swap(ILcoverAmount, 0, token1, token0);
+        ILcover += usdctoWETHConversion;
         emit Distributed(cashbackappreward);
     }
 
